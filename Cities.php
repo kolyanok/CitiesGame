fff622963cf440276e7506301b709d4c19d9aed3
kolyanok <kolyanok@users.noreply.github.com>
2019-09-07 16:04:37 +0300

    Code cleanup

--- Cities.php
+++ Cities.php
@@ -10,18 +10,14 @@ class Cities
 {
 	private $db = NULL;
 	private $chat = 0;
-	//private $search     = mb_strtolower($_GET['name'], 'UTF-8'); //переводим в нижний регист ответ
-	private $lines; //все города
+	private $lines; // cities list
 	private $citiescount;
-	private $used = array(); //использованные города
-	//private $fnameUsed  = 'usedCity.txt'; //ссылка на исп города
-	private $count1; //количество угаданных городов
-	private $count; //     = $count1--; //приравниваем к числу
-	private $otvet; //     = $used[$count1]; //текущий ответ
-	private $NUJbuk; //    = $this->checkLetter($used[$count1]) ; //посл буква нужная ОНА В ФАЙЛЕ
-	private $poslBukva;// = mb_substr($search, -1, 1, 'UTF-8'); //посл буква присланного овета
-	private $pervBukva;//  = mb_substr($search, 0, 1, 'UTF-8'); //первая буква присланного ответа
-	private $namedletters = array();//количество оставшихся городов на каждую букву
+	private $used = array(); // used cities
+	private $count1; // used cities count
+	private $count; //     = $count1--;
+	private $current; // current city
+	private $curletter; //    current letter
+	private $namedletters = array();// number of remaining cities per letter
 	function __construct($chatid)
 	{
 		require_once("config.php");
@@ -53,13 +49,12 @@ class Cities
 	
 	private function checkLetter($word)
 	{
-		//проверка на буквы
 		for ($i=mb_strlen($word, 'UTF-8'); $i>0; $i--)
 		{
-			$NOVbuk = mb_substr($word, $i-1, 1, 'UTF-8');
-			if ($this->namedletters[mb_strtoupper($NOVbuk, 'UTF-8')]>0)
+			$letter = mb_substr($word, $i-1, 1, 'UTF-8');
+			if ($this->namedletters[mb_strtoupper($letter, 'UTF-8')]>0)
 			{
-				return $NOVbuk;
+				return $letter;
 			}
 		}
 		return 'а';
@@ -79,8 +74,8 @@ class Cities
 				$this->used = unserialize($res['named']);
 				$this->count1 = count($this->used);
 				$this->count = $this->count1--;
-				$this->otvet = $this->used[$this->count1];
-				$this->NUJbuk = mb_strtoupper($this->checkLetter($this->used[$this->count1]));
+				$this->current = $this->used[$this->count1];
+				$this->current = mb_strtoupper($this->checkLetter($this->used[$this->count1]));
 				$game = true;
 			}
 			$result->close();
@@ -105,9 +100,6 @@ class Cities
 	
 	public function createGame()
 	{
-		//for ($i=ord('а'); $i<=ord('я'); $i++)
-		//	$this->namedletters[$i]=0;
-		//$this->namedletters=array_values($this->namedletters);
 		$letters = array('А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я');
 		foreach ($letters as $letter)
 		{
@@ -126,7 +118,6 @@ class Cities
 		$this->used[0] = $this->lines[rand(0,$this->citiescount-1)];
 		$this->namedletters[mb_substr($this->used[0], 0, 1, 'UTF-8')]--;
 		$newletter = $this->checkLetter($this->used[0]);
-		//die (var_dump($this->namedletters));
 		try
 		{
 			$this->db->query("INSERT INTO cities SET chatid = $this->chat, named='".$this->db->real_escape_string(serialize($this->used))."', letters='".$this->db->real_escape_string(serialize($this->namedletters))."'");
@@ -142,37 +133,23 @@ class Cities
 	{
 		if (!$this->getGame())
 			return $this->createGame();
-		/*$search     = mb_strtolower($_GET['name'], 'UTF-8'); //переводим в нижний регист ответ
-		$lines      = file('goroda.txt'); //все города
-		$used       = file('usedCity.txt'); //использованные города
-		$fnameUsed  = 'usedCity.txt'; //ссылка на исп города
-		$count1     = count($used); //количество угаданных городов
-		$count      = $count1--; //приравниваем к числу
-		//echo $count1;
-		$otvet      = $used[$count1]; //текущий ответ
-		$NUJbuk     = $this->checkLetter($used[$count1]) ; //посл буква нужная ОНА В ФАЙЛЕ
-		$poslBukva  = mb_substr($search, -1, 1, 'UTF-8'); //посл буква присланного овета
-		$pervBukva  = mb_substr($search, 0, 1, 'UTF-8'); //первая буква присланного ответа
-		*/
-		$search     = mb_strtolower($turn, 'UTF-8'); //переводим в нижний регист ответ
-		$pervBukva  = mb_strtoupper(mb_substr($search, 0, 1, 'UTF-8'));
+		$search     = mb_strtolower($turn, 'UTF-8');
+		$firstletter  = mb_strtoupper(mb_substr($search, 0, 1, 'UTF-8'));
 		foreach ($this->used as $us) {
 			$us = mb_strtolower($us, 'UTF-8');
-		    if (strpos($us, $search) !== false and mb_strlen($us, 'UTF-8') < mb_strlen($search, 'UTF-8') + 2) { //проверка был ли город
-		        return 'Город '. mb_strtoupper($search, 'UTF-8'). ' уже называли! Назовите другой город на букву '. $this->NUJbuk;
-		        //exit;
+		    if (strpos($us, $search) !== false and mb_strlen($us, 'UTF-8') < mb_strlen($search, 'UTF-8') + 2) { //check if this city is already named 
+		        return 'Город '. mb_strtoupper($search, 'UTF-8'). ' уже называли! Назовите другой город на букву '. $this->current;
 		    }
 		}
 		
 		if (mb_strlen($search) <= 2) {
-		    return 'Текущий город: '. mb_strtoupper($this->otvet, 'UTF-8'). ' Первая буква города должна быть '. $this->NUJbuk;
-		    //exit;
+		    return 'Текущий город: '. mb_strtoupper($this->current, 'UTF-8'). ' Первая буква города должна быть '. $this->current;
 		}
 		
 		$fo    = mb_strlen($search, 'UTF-8') - 1;
 		$found = false;
 
-		if (mb_strpos($this->NUJbuk, $pervBukva) !== false) //проверка совпадает ли первая буква с последней
+		if (mb_strpos($this->current, $firstletter) !== false) //проверка совпадает ли первая буква с последней
 		    {
 		    foreach ($this->lines as $line) 
 		    {
@@ -181,16 +158,15 @@ class Cities
 		            {
 		            $found = true;
 		            $this->count++;
-		            if ($this->count >= 30) //если количество городов равно 30
+		            if ($this->count >= 30)
 		                {
 		                $this->removeGame();
 		                return "30-ый город назван. Игра окончена!";
-		            	} else { //если городов меньше 30
-		                //проверка на буквы ыыы ййй ььь ёёёё  цццц  ффффф щщщ цикл ОФР ФОР
-		               	$NOVbuk = $this->checkLetter ($fndcity);
+		            	} else { 
+		               	$newletter = $this->checkLetter ($fndcity);
 		               	$this->namedletters[mb_substr($fndcity, 0, 1, 'UTF-8')]--;
 		               	$this->used[]=$fndcity;
-		                } //konec while
+		                } 
 		                try
 										{
 											$this->db->query("UPDATE cities SET named='".$this->db->real_escape_string(serialize($this->used))."', letters='".$this->db->real_escape_string(serialize($this->namedletters))."' WHERE chatid=$this->chat");
@@ -199,18 +175,13 @@ class Cities
 										{
 												 die ("База данных недоступна");	
 										}
-		                return 'Назван правильный город '. mb_strtoupper($fndcity, 'UTF-8'). '! Теперь нужно назвать город на букву '. mb_strtoupper($NOVbuk, 'UTF-8');
+		                return 'Назван правильный город '. mb_strtoupper($fndcity, 'UTF-8'). '! Теперь нужно назвать город на букву '. mb_strtoupper($newletter, 'UTF-8');
 		                
 		            }
-		            //не в ИФ, потому что всё равно пишем кол-во, или нулевое, или нет
-		           // break; //прерываем поиск
 		        }
 		    }
-		    // If the text was not found, show a message
+		    // If the city was not found, show a message
 		    if (!$found)
-		        return 'Такого города в России нет! Текущий город: '. mb_strtoupper($this->used[$this->count1], 'UTF-8'). ' Первая буква города должна быть '. $this->NUJbuk;
+		        return 'Такого города в России нет! Текущий город: '. mb_strtoupper($this->used[$this->count1], 'UTF-8'). ' Первая буква города должна быть '. $this->current;
 		}
 }
-//$cities = new Cities(1);
-//$cities->removeGame();
-//echo $cities->nextStep($_GET['name']);
