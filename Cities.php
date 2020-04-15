09df826d40d6af9d9df4dbd4abac3322a5632213
kolyanok <kolyan-okunok@ya.ru>
2020-04-16 02:11:04 +0300

    Remove 30 cities limit + bug fix

--- Cities.php
+++ Cities.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 if (!function_exists('mb_ucfirst')) {
     function mb_ucfirst($string, $enc = 'UTF-8')
     {
@@ -27,25 +27,25 @@ class Cities
         $this->lines       = file("goroda.txt");
         $this->citiescount = count($this->lines);
         if ($this->citiescount < 1)
-             throw new Exception('DB problems');
+            throw new Exception('DB problems');
         for ($i = 0; $i < $this->citiescount; $i++)
             $this->lines[$i] = mb_ucfirst(trim($this->lines[$i]), 'UTF-8');
         try {
             $this->db = new mysqli($my_host, $my_user, $my_password, $my_database);
             $this->db->set_charset('utf8');
             $this->db->query('SET NAMES utf8');
-            
+
         }
         catch (Exception $e) {
             throw new Exception('DB problems');
         }
     }
-    
+
     function __destruct()
     {
         $this->db->close();
     }
-    
+
     private function checkLetter($word)
     {
         for ($i = mb_strlen($word, 'UTF-8'); $i > 0; $i--) {
@@ -54,9 +54,9 @@ class Cities
                 return $letter;
             }
         }
-        return 'а';
+        return NULL;
     }
-    
+
     public function getGame()
     {
         $game = false;
@@ -70,7 +70,10 @@ class Cities
                 $this->count        = $this->count1--;
                 $this->current      = $this->used[$this->count1];
                 $this->current      = mb_strtoupper($this->checkLetter($this->used[$this->count1]));
-                $game               = true;
+                if (is_null($this->current))
+                    $this->removeGame();
+                else
+                    $game               = true;
             }
             $result->close();
         }
@@ -79,7 +82,7 @@ class Cities
         }
         return $game;
     }
-    
+
     public function removeGame()
     {
         try {
@@ -89,7 +92,7 @@ class Cities
             throw new Exception('DB problems');
         }
     }
-    
+
     public function createGame()
     {
         $letters = array('А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я');
@@ -115,7 +118,7 @@ class Cities
         }
         return "Начинаю новую игру: " . mb_ucfirst($this->used[0]) . "\n Назовите город на букву " . mb_strtoupper($newletter);
     }
-    
+
     public function nextStep($turn)
     {
         if (!$this->getGame())
@@ -124,42 +127,41 @@ class Cities
         $firstletter = mb_strtoupper(mb_substr($search, 0, 1, 'UTF-8'));
         foreach ($this->used as $us) {
             $us = mb_strtolower($us, 'UTF-8');
-            if (strpos($us, $search) !== false and mb_strlen($us, 'UTF-8') < mb_strlen($search, 'UTF-8') + 2) { //check if this city is already named 
+            if (strpos($us, $search) !== false and mb_strlen($us, 'UTF-8') < mb_strlen($search, 'UTF-8') + 2) { //check if this city is already named
                 return 'Город ' . mb_strtoupper($search, 'UTF-8') . ' уже называли! Назовите другой город на букву ' . $this->current;
             }
         }
-        
+
         if (mb_strlen($search) <= 2) {
-            return 'Текущий город: ' . mb_strtoupper($this->current, 'UTF-8') . ' Первая буква города должна быть ' . $this->current;
+            return 'Текущий город: ' . mb_strtoupper($this->used[$this->count1], 'UTF-8') . ' Первая буква города должна быть ' . $this->current;
         }
-        
+
         $fo    = mb_strlen($search, 'UTF-8') - 1;
         $found = false;
-        
+
         if (mb_strpos($this->current, $firstletter) !== false) //check if the letter is correct
-            {
+        {
             foreach ($this->lines as $line) {
                 $fndcity = trim($line);
                 if (mb_strtolower($fndcity) === mb_strtolower($search)) //if the city exists
-                    {
+                {
                     $found = true;
                     $this->count++;
-                    if ($this->count >= 30) {
-                        $this->removeGame();
-                        return "30-ый город назван. Игра окончена!";
-                    } else {
-                        $newletter = $this->checkLetter($fndcity);
-                        $this->namedletters[mb_substr($fndcity, 0, 1, 'UTF-8')]--;
-                        $this->used[] = $fndcity;
-                    }
+                    $newletter = $this->checkLetter($fndcity);
+                    $this->namedletters[mb_substr($fndcity, 0, 1, 'UTF-8')]--;
+                    $this->used[] = $fndcity;
                     try {
                         $this->db->query("UPDATE cities SET named='" . $this->db->real_escape_string(serialize($this->used)) . "', letters='" . $this->db->real_escape_string(serialize($this->namedletters)) . "' WHERE chatid=$this->chat");
                     }
                     catch (Exception $e) {
                         throw new Exception('DB problems');
                     }
-                    return 'Назван правильный город ' . mb_strtoupper($fndcity, 'UTF-8') . '! Теперь нужно назвать город на букву ' . mb_strtoupper($newletter, 'UTF-8');
-                    
+                    if (is_null($this->checkLetter ($newletter))) {
+                        $this->removeGame();
+                        return "Не осталось городов. Игра окончена!";
+                    } else {
+                        return 'Назван правильный город ' . mb_strtoupper($fndcity, 'UTF-8') . '! Теперь нужно назвать город на букву ' . mb_strtoupper($newletter, 'UTF-8');
+                    }
                 }
             }
         }
